57309b44aa52284745aab757ce7ab76eb21d50c0
Mudabbirjairajpuri <69682228+Mudabbirjairajpuri@users.noreply.github.com>
2020-11-22 11:36:21 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
 print("Hello Mudabbir Jairajpuri")
 print("Welcome to TRCAC")
-print("We Hope You will perform well in Studies")
+print("We Hope You will Perform well in Studies")
+print("We Hope You will Respect All Teachers and Friends")
